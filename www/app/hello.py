44cfdc55d49c27b87ed3ddb9f9dd36b3d68ae519
fangangmin <fangangmin@gmail.com>
2017-08-18 10:52:19 +0800

    update:上线版本

--- www/app/hello.py
+++ www/app/hello.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flaskext.mysql import MySQL
 import json
-import random, time
+import random, time, copy
 import threading
 
 mysql = MySQL()
@@ -12,9 +12,8 @@ app.config['MYSQL_DATABASE_DB'] = 'inst'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-max = 0
-min = 0
-stars = []
+size = 20
+
 
 @app.route("/")
 def hello():
@@ -23,10 +22,22 @@ def hello():
 
 @app.route("/wximg/")
 def wximg():
-    interval_init()
+    global size
     cursor = mysql.connect().cursor()
+    max, min, stars = interval_init(cursor)
     name = request.args.get('name')
-    # choose_star = random.choice(stars)
+    datas = []
+    for i in range(size):
+        one = get_one_data(max, min, stars, cursor)
+        datas.append(one)
+    j = json.dumps({
+        'data': datas
+    })
+    cursor.close()
+    return j
+
+
+def get_one_data(max, min, stars, cursor):
     target_id = int((max - min) * random.random() + min)
     sql = 'SELECT name,qiniu_url,id FROM instagram WHERE id = ' + str(target_id) + ' ORDER BY id LIMIT 1;'
     cursor.execute(sql)
@@ -38,43 +49,35 @@ def wximg():
     result2 = cursor.fetchone()
     cn_name = result2[0]
     main_page = result2[1]
-    random_star = random.sample(stars, 4)
-    j = json.dumps({
-        'data': {
-            'name': cn_name,
-            'imgurl': imgurl,
-            'main_page': main_page,
-            'random_star':random_star
-        }
-    })
-    cursor.close()
-    return j
+    small_stars = copy.copy(stars)
+    small_stars.remove(cn_name)
+    random_star = random.sample(small_stars, 3)
+    url_type = 'mp4' if ('.mp4' in imgurl) else 'jpg'
+    one = {
+        'name': cn_name,
+        'imgurl': imgurl,
+        'main_page': main_page,
+        'random_star': random_star,
+        'type': url_type
+    }
+    return one
 
 
-def interval_init():
-    global stars
-    cursor = mysql.connect().cursor()
+def interval_init(cursor):
+    max = 0
+    min = 0
+    stars = []
     star_sql = 'select cn_name from star'
     cursor.execute(star_sql)
     for s in cursor.fetchall():
         stars.append(s[0])
-    if True:
-        global max
-        global min
-        cursor.execute('SELECT MAX(id) FROM instagram')
-        max = cursor.fetchone()[0]
-        cursor.execute('SELECT MIN(id) FROM instagram')
-        min = cursor.fetchone()[0]
-        print 'max:', max, '=====min:', min, '=====len(stars):', str(len(stars))
-#        time.sleep(3600)
-    cursor.close()
-
-
-def init_data():
-    t = threading.Thread(target=interval_init)
-    t.start()
+    cursor.execute('SELECT MAX(id) FROM instagram')
+    max = cursor.fetchone()[0]
+    cursor.execute('SELECT MIN(id) FROM instagram')
+    min = cursor.fetchone()[0]
+    # print 'max:', max, '=====min:', min, '=====len(stars):', str(len(stars))
+    return max, min, stars
 
 
 if __name__ == '__main__':
-   # init_data()
     app.run(debug=True)
